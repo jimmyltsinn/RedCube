2f0c5a4b6fb04404b452a748e9b761e4c0c2602e
Jimmy Sinn <jimmyltsinn@gmail.com>
2017-05-10 20:11:30 +0800

    Add simple UI elements to control spinning of the cube

--- Assets/Spin.cs
+++ Assets/Spin.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI; 
+
 public class Spin : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	public float speed = 90; 
+	bool rotate = true; 
+
+	public void buttonPressed(Button myObject) {
+		rotate = !rotate; 
+		for (int i = 0; i < myObject.transform.childCount; ++i) {
+			Debug.Log (myObject.transform.GetChild (i).name);
+			if (myObject.transform.GetChild(i).transform.name == "Text") {
+				myObject.transform.GetChild (i).transform.GetComponent<Text>().text = rotate ? "ON" : "OFF"; 
+			}
+		}
+	}
+
+	public void updateSpeed(float val) {
+		speed = val * 90; 
+	}
+
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(Vector3.up * Time.deltaTime*90);
+		if (rotate)
+			transform.Rotate(Vector3.up * Time.deltaTime * speed);
 	}
 }
